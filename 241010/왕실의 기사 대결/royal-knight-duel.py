05320564bf8c0df71ea6b3852475b7ca970f189b
bangjyun <112922392+bangjyun@users.noreply.github.com>
2024-10-10 20:31:02 +0900

    Title: 왕실의 기사 대결, Time: 210ms, Memory: 26MB, Status: Passed - CodeTree

--- 241010/왕실의 기사 대결/royal-knight-duel.py
+++ 241010/왕실의 기사 대결/royal-knight-duel.py
@@ -1,12 +1,12 @@
 L,N,Q=map(int,input().split())
 arr= [[2]*(L+2)]+[[2]+list(map(int,input().split()))+[2] for _ in range(L)]+[[2]*(L+2)]
 bomb = set()
-for i in range(L):
-    for j in range(L):
+for i in range(L+2):
+    for j in range(L+2):
         if arr[i][j]==1: # 함정
             bomb.add((i,j))
             arr[i][j]=0
-
+out=[0]*(N+1)
 loard={}
 for m in range(1,N+1):
     r,c,h,w,k=map(int,input().split())
@@ -18,6 +18,9 @@ for m in range(1,N+1):
 Qlst=[list(map(int,input().split())) for _ in range(Q)]
 qdi=[-1,0,1,0]
 qdj=[0,1,0,-1]
+
+def pprint(arr):
+    print("\n".join(" ".join(f"{e:>3}" for e in l) for l in arr))
 
 from collections import deque
 def move_loard(idx):
@@ -36,6 +39,7 @@ def move_loard(idx):
                     return False
                 elif -31< arr[ni][nj]<0 and arr[ni][nj]!=arr[ci][cj] : # 다른 기사가 있는 경우 (나 말고)
                     nidx= - arr[ni][nj]
+                    if out[nidx]: continue  # 그 인덱스 없어!
                     q.append(nidx)
                     fset.add(nidx)
     return True
@@ -43,23 +47,42 @@ def move_loard(idx):
 
 for qi,qd in Qlst: # 명령받은 기사 인덱스, 방향
     fset=set()
+    # print(f"============={qi,qd}==========")
+    if out[qi]: continue # 그 인덱스 없어!
+    if len(loard)==0: # 기사 전멸
+        break
+
+    narr=[x[:] for x in arr]
     if move_loard(qi): # 이동 시작 (이동 가능하니 벽은 상관 X)
         for fidx in fset:  # 해당하는 기사들
             fi, fj, fh, fw, fk, fdmg = loard[fidx]
-            fi,fj=fi+qdi[qd], fj + qdj[qd] # 좌상단 좌표 업데이트
             for i in range(fi, fi + fh):
-                for j in range(fi, fj + fw):
+                for j in range(fj, fj + fw): # 동시에 이동
+                    narr[i][j] = 0
+
+        for fidx in fset:  # 해당하는 기사들
+            fi, fj, fh, fw, fk, fdmg = loard[fidx]
+            for i in range(fi, fi + fh):
+                for j in range(fj, fj + fw):  # 동시에 이동
                     ni,nj= i+qdi[qd], j + qdj[qd] # 이동
-                    if (ni,nj) in bomb: # 함정이 있으면
+                    narr[ni][nj]=arr[i][j]
+                    # arr[ni][nj]=-fidx # arr에 표시
+                    if (ni,nj) in bomb and fidx!=qi: # 함정이 있으면, !!!!! 놓침: 명령받은 기사는 피해를 입지 않는다
                         fdmg+=1
                         fk-=1
-                    arr[i][j]=0 # 기존 격자는 비우고 이동
-                    arr[ni][nj]=-fidx # arr에 표시
+            fi, fj = fi + qdi[qd], fj + qdj[qd]  # 좌상단 좌표 업데이트
             loard[fidx]=fi, fj, fh, fw, fk, fdmg # 업데이트
+        arr = narr
+        for fidx in fset:  # 해당하는 기사들
+            fi, fj, fh, fw, fk, fdmg = loard[fidx]
             if fk<=0: # 체력 바닥남
-                loard.pop(fi) # 삭제해버리기
-    else: # 벽이 있어 이동 불가
-        pass
+                out[fidx]=1
+                loard.pop(fidx) # 삭제해버리기
+                for i in range(fi, fi + fh):
+                    for j in range(fj, fj + fw):
+                        arr[i][j]=0
+
+    # pprint(arr)
 
 tot_dmg=0
 for live_loard in loard.values():
