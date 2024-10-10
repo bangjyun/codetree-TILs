fdc0c2ec6eaac7d6754928d0f294ecfc31dd9241
bangjyun <112922392+bangjyun@users.noreply.github.com>
2024-10-10 20:53:19 +0900

    Title: 왕실의 기사 대결, Time: 232ms, Memory: 26MB, Status: Passed - CodeTree

--- 241010/왕실의 기사 대결/royal-knight-duel.py
+++ 241010/왕실의 기사 대결/royal-knight-duel.py
@@ -57,22 +57,17 @@ for qi,qd in Qlst: # 명령받은 기사 인덱스, 방향
         for fidx in fset:  # 해당하는 기사들
             fi, fj, fh, fw, fk, fdmg = loard[fidx]
             for i in range(fi, fi + fh):
-                for j in range(fj, fj + fw): # 동시에 이동
-                    narr[i][j] = 0
-
-        for fidx in fset:  # 해당하는 기사들
-            fi, fj, fh, fw, fk, fdmg = loard[fidx]
-            for i in range(fi, fi + fh):
                 for j in range(fj, fj + fw):  # 동시에 이동
-                    ni,nj= i+qdi[qd], j + qdj[qd] # 이동
-                    narr[ni][nj]=arr[i][j]
-                    # arr[ni][nj]=-fidx # arr에 표시
-                    if (ni,nj) in bomb and fidx!=qi: # 함정이 있으면, !!!!! 놓침: 명령받은 기사는 피해를 입지 않는다
-                        fdmg+=1
-                        fk-=1
+                    ni, nj = i + qdi[qd], j + qdj[qd]  # 이동
+                    narr[i][j] -= arr[i][j]
+                    narr[ni][nj] += arr[i][j]
+                    if (ni, nj) in bomb and fidx != qi:  # 함정이 있으면, !!!!! 놓침: 명령받은 기사는 피해를 입지 않는다
+                        fdmg += 1
+                        fk -= 1
             fi, fj = fi + qdi[qd], fj + qdj[qd]  # 좌상단 좌표 업데이트
-            loard[fidx]=fi, fj, fh, fw, fk, fdmg # 업데이트
+            loard[fidx] = fi, fj, fh, fw, fk, fdmg  # 업데이트
         arr = narr
+
         for fidx in fset:  # 해당하는 기사들
             fi, fj, fh, fw, fk, fdmg = loard[fidx]
             if fk<=0: # 체력 바닥남
@@ -81,8 +76,6 @@ for qi,qd in Qlst: # 명령받은 기사 인덱스, 방향
                 for i in range(fi, fi + fh):
                     for j in range(fj, fj + fw):
                         arr[i][j]=0
-
-    # pprint(arr)
 
 tot_dmg=0
 for live_loard in loard.values():
